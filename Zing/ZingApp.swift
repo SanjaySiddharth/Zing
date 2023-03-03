1131946a4181b94fcfd3623aa9f1d0d3420f8027
SanjaySiddharth <mjsanjaysiddharth1999@gmail.com>
2023-03-03 11:54:49 +0530

    Initial Commit

---
+++ Zing/ZingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZingApp.swift
+//  Zing
+//
+//  Created by Sanjay Siddharth on 03/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ZingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
